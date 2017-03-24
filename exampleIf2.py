64eb5b14578e7c4c4f7f81971601cb1d00d29b1a
codemasterram <codemaster.ram@gmail.com>
2017-03-24 10:13:33 +0545

    if and elsefif statments check

---
+++ exampleIf2.py
@@ -0,0 +1,21 @@
+word=input('Word:')
+if word == "Foo":
+	print('Great')
+	print('You entered "Foo"')
+elif word == "Ram":
+	print('Great')
+	print('You entered "Ram"')
+elif word == "Bar":
+	print('Great')
+	print('You entered "Bar"')
+elif word == "Baz":
+	print('Great')
+	print('You entered "Baz"')
+else:
+	print('Hey!')
+	print('You entered {}'.format(word))
+
+# Normal statements out of the block
+
+print('Good Bye')
+
