a8a1183e2dc5abd58e4b5294d2fceb0e689512e8
codemasterram <codemaster.ram@gmail.com>
2017-03-26 14:05:38 +0545

    Function and its examples are inside these files..

---
+++ day2-3.py
@@ -0,0 +1,23 @@
+user = [
+{
+	'name':'Ram Sapkota',
+	'email':'codemaster.ram@gmail.com',
+	'phone':'9840019874',
+	'address':'imadole lalitpur Nepal'
+},
+{
+	'name':'Ram Sapkota',
+	'email':'codemaster.ram@gmail.com',
+	'phone':'9840019874',
+	'address':'imadole lalitpur Nepal'
+},
+{
+	'name':'Ram Sapkota',
+	'email':'codemaster.ram@gmail.com',
+	'phone':'9840019874',
+	'address':'imadole lalitpur Nepal'
+}
+]
+
+for (key,val) in user[0].items():
+	print(' %s : %s' % (key, val))
